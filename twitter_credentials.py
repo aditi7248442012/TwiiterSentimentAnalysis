ad5c312d137010de0156c474303142d2adef218d
aditi7248442012 <46889916+aditi7248442012@users.noreply.github.com>
2020-07-19 03:53:48 -0700

    Add files via upload

---
+++ twitter_credentials.py
@@ -0,0 +1,8 @@
+#these credentials are kept secret because anyone can access my twitter application.
+# the secret token are to be written inside inverted commas
+# and this file is imported in sentiment_analysis.py
+
+ACCESS_TOKEN = " "
+ACCESS_TOKEN_SECRET = " "
+CONSUMER_KEY = " "
+CONSUMER_SECRET = " "
